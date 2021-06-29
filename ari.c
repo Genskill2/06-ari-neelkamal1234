069e0d07c7bf244f199f6015300acfa57a14fa08
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-30 01:00:40 +0530

    NC1-1

--- ari.c
+++ ari.c
@@ -4,9 +4,9 @@
 #include <ctype.h>
 
 string ari(string s)
-{  int count1 =0;
-   int count2 =0;
-   int count3 =0; 
+{  int c1 =0;
+   int c2 =0;
+   int c3 =0; 
  int n = strlen(s);
    
  
@@ -14,58 +14,58 @@ string ari(string s)
      
      { 
        if ( isalnum(s[i])==1)
-       { count1+=1;}
+       { c1+=1;}
  
        
         if( s[i] ==' ')
-        { count2+=1;}
+        { c2+=1;}
                      
                
          if( s[i]=='.'|| s[i]=='?')
-         { count3+=1; }
+         { c3+=1; }
                   
             
-     } count2+=1;
+     } c2+=1;
  
   
    for( int j=0;j<n;j++)
        {
          if(s[j]=='e'&&s[j+1]=='.'&&s[j+2]=='g'&&s[j+3]=='.')
-         { count3 = count3-1;}
+         { c3 = c3-1;}
        }
       
-      float ARI;
-      int ari1;
-      string st;
-      float a1 =(float)count1/count2;
-      float a2 = (float)count2/count3;
+      float R;
+      int r;
+      string t;
+      float a1 =(float)c1/c2;
+      float a2 = (float)c2/c3;
       
-      ARI = 4.71*a1 + 0.5*a2 -21.43 ;
+      R = 4.71*a1 + 0.5*a2 -21.43 ;
                            
-      ari1 =(int)(ARI+1.0000); 
+      r =(int)(R+1.0000); 
  
-      switch(ari1)
+      switch(r)
       {
-        case 1 : st ="kindergarten";break;
-        case 2 : st ="First/Second Grade";break;
-        case 3 : st ="Third Grade";break;
-        case 4 : st ="Fourth Grade";break;  
-        case 5 : st ="Fifth Grade";break;
-        case 6 : st ="Sixth Grade";break;
-        case 7 : st ="Seventh Grade";break;
-        case 8 : st ="Eighth Grade";break;      
-        case 9 : st ="Ninth Grade";break;                 
-        case 10 : st ="Tenth Grade";break; 
-        case 11 : st ="Eleventh Grade";break;                 
-        case 12 : st ="Twelfth Grade";break;               
-        case 13 : st ="College Student";break;                
-        case 14 : st ="Professor";break;                  
+        case 1 : t ="kindergarten";break;
+        case 2 : t ="First/Second Grade";break;
+        case 3 : t ="Third Grade";break;
+        case 4 : t ="Fourth Grade";break;  
+        case 5 : t ="Fifth Grade";break;
+        case 6 : t ="Sixth Grade";break;
+        case 7 : t ="Seventh Grade";break;
+        case 8 : t ="Eighth Grade";break;      
+        case 9 : t ="Ninth Grade";break;                 
+        case 10 : t ="Tenth Grade";break; 
+        case 11 : t ="Eleventh Grade";break;                 
+        case 12 : t ="Twelfth Grade";break;               
+        case 13 : t ="College Student";break;                
+        case 14 : t ="Professor";break;                  
         
         
         }
         
         
-         return st;
+         return t;
         }
               
            
