a6ec93bd5799b9a3dbb0f65c4190510069d11c8f
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-29 15:11:56 +0530

    Made new changes

--- ari.c
+++ ari.c
@@ -7,9 +7,13 @@ char *ari( char *s)
    for(int i=0;s[i]!='\0';i++)
      
      { 
-       if ( isalnum(s[i])|| isalpha(s[i]) )
+       if ( s[i]>=48 && s[i]<=57)
             count1++;
-            
+      
+      else if( (s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122) )
+               count1++;
+      
+      
             else{
                   if( s[i] =='_' )
                      count2++;
@@ -36,20 +40,20 @@ char *ari( char *s)
         
       switch(ari1)
       {
-        case 1 : st ="kindergarten";break;
-        case 2 : st ="First/Second Grade";break;
-        case 3 : st ="Third Grade";break;
-        case 4 : st ="Fourth Grade";break;  
-        case 5 : st ="Fifth Grade";break;
-        case 6 : st ="Sixth Grade";break;
-        case 7 : st ="Seventh Grade";break;
-        case 8 : st ="Eighth Grade";break;      
-        case 9 : st ="Ninth Grade";break;                 
-        case 10 : st ="Tenth Grade";break; 
-        case 11 : st ="Eleventh Grade";break;                 
-        case 12 : st ="Twelfth Grade";break;               
-        case 13 : st ="College Student";break;                
-        case 14 : st ="Professor";break;                  
+        case 1 : strcpy(st,"kindergarten");break;
+        case 2 : strcpy(st,"First/Second Grade");break;
+        case 3 : strcpy(st,"Third Grade");break;
+        case 4 : strcpy(st,"Fourth Grade");break;  
+        case 5 : strcpy(st,"Fifth Grade");break;
+        case 6 : strcpy(st,"Sixth Grade");break;
+        case 7 : strcpy(st,"Seventh Grade");break;
+        case 8 : strcpy(st,"Eighth Grade");break;      
+        case 9 : strcpy(st,"Ninth Grade");break;                 
+        case 10 : strcpy(st,"Tenth Grade");break; 
+        case 11 : strcpy(st,"Eleventh Grade");break;                 
+        case 12 : strcpy(st,"Twelfth Grade");break;               
+        case 13 : strcpy(st,"College Student");break;                
+        case 14 : strcpy(st,"Professor");break;                  
         default :printf( " Invalid Choice \n" )
         
         }
